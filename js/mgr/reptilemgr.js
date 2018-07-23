827a316094223f4dcfc154826fdddc9e6a212fe8
lucifer_dc <942316156@qq.com>
2018-07-23 18:42:57 +0800

    modify

---
+++ js/mgr/reptilemgr.js
@@ -0,0 +1,26 @@
+exports.get2 = function (url,data,callback,safe) {
+	var content = qs.stringify(data);
+	var url = url + '?' + content;
+	var proto = http;
+	if(safe){
+		proto = https;
+	}
+	var req = proto.get(url, function (res) {  
+		//console.log('STATUS: ' + res.statusCode);  
+		//console.log('HEADERS: ' + JSON.stringify(res.headers));  
+		res.setEncoding('utf8');  
+		res.on('data', function (chunk) {  
+			console.log('BODY: ' + chunk);
+            // var json = JSON.parse(chunk);
+            // console.log(chunk);
+			callback(true,chunk);
+		});  
+	});
+	  
+	req.on('error', function (e) {  
+		console.log('problem with request: ' + e.message);
+		callback(false,e);
+	});  
+	  
+	req.end(); 
+};
